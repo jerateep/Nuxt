17e30e06457097e9abac5cf1571b65370d41b428
jerateeps <jerateeps@thaicom.net>
2020-07-15 00:01:44 +0700

    use asset/images

---
+++ components/posts/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-carousel id="Slider" controls indicators>
+      <div v-for="item in items">
+        <b-carousel-slide
+          :caption=item.caption
+          :text=item.text
+          :img-src=item.image>
+        </b-carousel-slide>
+      </div>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      items:[
+        {caption:"A",image:require("@/assets/images/pic1.jpg"),text:"test 1"}
+        ,{caption:"B",image:"https://picsum.photos/1024/480/?image=38",text:"test 2"}
+        ,{caption:"C",image:"https://picsum.photos/1024/480/?image=32",text:"test 3"}
+        ,{caption:"D",image:"https://picsum.photos/1024/480/?image=33",text:"test 4"}
+        ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Posts</h1>
+  <b-container fluid>
+    <Logo/>
+    <h1>Posts</h1>
+    <Header/>
+    <b-row class="py-2">
+      <PostContant/>
+      <PostContant/>
+      <PostContant/>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import Logo from '@/components/Logo'
+import Header from '@/components/posts/Header'
+import PostContant from '@/components/posts/PostContant'
 export default {
-  layout:"coreLayout"
+  layout:"coreLayout",
+  components:{
+    Header
+    , Logo
+    , PostContant
+  }
 }
 </script>
 
